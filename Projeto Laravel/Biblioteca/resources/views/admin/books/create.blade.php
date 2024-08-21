c27979e75cf67bdc70e87df6db2462ca7e9e28ab
TheuZCoder <matheusrdsilva1@gmail.com>
2024-08-21 20:04:13 -0300

    ajustes do css

--- Projeto Laravel/Biblioteca/resources/views/admin/books/create.blade.php
+++ Projeto Laravel/Biblioteca/resources/views/admin/books/create.blade.php
@@ -1,27 +1,30 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <h1>Add New Book</h1>
+<div class="containerFora">
+    <div class="pedit">
+        <h1>Add New Book</h1>
+    </div>
+    <div class="cardedit">
+        <form action="{{ route('admin.books.store') }}" method="POST">
+            @csrf
+            <div class="form-group">
+                <label for="title">Title</label>
+                <input type="text" name="title" id="title" class="form-control" required>
+            </div>
 
-    <form action="{{ route('admin.books.store') }}" method="POST">
-        @csrf
-        <div class="form-group">
-            <label for="title">Title</label>
-            <input type="text" name="title" id="title" class="form-control" required>
-        </div>
+            <div class="form-group">
+                <label for="author">Author</label>
+                <input type="text" name="author" id="author" class="form-control" required>
+            </div>
 
-        <div class="form-group">
-            <label for="author">Author</label>
-            <input type="text" name="author" id="author" class="form-control" required>
-        </div>
+            <div class="form-group">
+                <label for="genre">Genre</label>
+                <input type="text" name="genre" id="genre" class="form-control" required>
+            </div>
 
-        <div class="form-group">
-            <label for="genre">Genre</label>
-            <input type="text" name="genre" id="genre" class="form-control" required>
-        </div>
-
-        <button type="submit" class="btn btn-success">Add Book</button>
-    </form>
+            <button type="submit" class="btn btn-success">Add Book</button>
+        </form>
+    </div>
 </div>
 @endsection
--- Projeto Laravel/Biblioteca/resources/views/admin/books/edit.blade.php
+++ Projeto Laravel/Biblioteca/resources/views/admin/books/edit.blade.php
@@ -1,29 +1,34 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <h1>Edit Book</h1>
+<div class="containerFora">
+    <div class="pedit">
+        <h1>Edit Book</h1>
+    </div>
+    <div class="cardedit">
 
-    <form action="{{ route('admin.books.update', $book->id) }}" method="POST">
-        @csrf
-        @method('PUT')
 
-        <div class="form-group">
-            <label for="title">Title</label>
-            <input type="text" name="title" id="title" class="form-control" value="{{ $book->title }}" required>
-        </div>
+        <form action="{{ route('admin.books.update', $book->id) }}" method="POST">
+            @csrf
+            @method('PUT')
 
-        <div class="form-group">
-            <label for="author">Author</label>
-            <input type="text" name="author" id="author" class="form-control" value="{{ $book->author }}" required>
-        </div>
+            <div class="form-group">
+                <label for="title">Title</label>
+                <input type="text" name="title" id="title" class="form-control" value="{{ $book->title }}" required>
+            </div>
 
-        <div class="form-group">
-            <label for="genre">Genre</label>
-            <input type="text" name="genre" id="genre" class="form-control" value="{{ $book->genre }}" required>
-        </div>
+            <div class="form-group">
+                <label for="author">Author</label>
+                <input type="text" name="author" id="author" class="form-control" value="{{ $book->author }}" required>
+            </div>
 
-        <button type="submit" class="btn btn-primary">Update Book</button>
-    </form>
+            <div class="form-group">
+                <label for="genre">Genre</label>
+                <input type="text" name="genre" id="genre" class="form-control" value="{{ $book->genre }}" required>
+            </div>
+
+            <button type="submit" class="btn btn-primary">Update Book</button>
+        </form>
+    </div>
 </div>
 @endsection

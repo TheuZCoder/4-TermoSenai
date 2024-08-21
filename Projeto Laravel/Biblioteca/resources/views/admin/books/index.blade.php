e17717cfa3c83a6e83bc59d8b558765fc0ca1470
TheuZCoder <matheusrdsilva1@gmail.com>
2024-08-20 21:00:27 -0300

    feito algumas alterações

--- Projeto Laravel/Biblioteca/resources/views/admin/books/index.blade.php
+++ Projeto Laravel/Biblioteca/resources/views/admin/books/index.blade.php
@@ -2,13 +2,15 @@
 
 @section('content')
 <div class="DivFora">
-    <h1>Manage Books</h1>
-    <a href="{{ route('admin.books.create') }}" class="btn btn-success mb-3">Add New Book</a>
+    <div class="top">
+        <h1>Manage Books</h1>
+        <a href="{{ route('admin.books.create') }}" class="btn btn-primary custom-button">Add New Book</a>
+    </div>
 
     @if(session('success'))
-        <div class="alert alert-success">
-            {{ session('success') }}
-        </div>
+    <div class="alert alert-success">
+        {{ session('success') }}
+    </div>
     @endif
 
     <table class="table">
@@ -23,15 +25,15 @@
         <tbody>
             @foreach($books as $book)
             <tr>
-                <td>{{ $book->title }}</td>
-                <td>{{ $book->author }}</td>
-                <td>{{ $book->genre }}</td>
+                <td class="title">{{ $book->title }}</td>
+                <td class="autor">{{ $book->author }}</td>
+                <td class="genre">{{ $book->genre }}</td>
                 <td>
-                    <a href="{{ route('admin.books.edit', $book->id) }}" class="btn btn-primary">Edit</a>
+                    <a href="{{ route('admin.books.edit', $book->id) }}" class="btn btn-edit">Edit</a>
                     <form action="{{ route('admin.books.destroy', $book->id) }}" method="POST" class="d-inline">
                         @csrf
                         @method('DELETE')
-                        <button type="submit" class="btn btn-danger" onclick="return confirm('Are you sure?')">Delete</button>
+                        <button type="submit" class="btn btn-delete" onclick="return confirm('Are you sure?')">Delete</button>
                     </form>
                 </td>
             </tr>

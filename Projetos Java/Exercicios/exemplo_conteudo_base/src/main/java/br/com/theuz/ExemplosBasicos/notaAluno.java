16c49a6f97e963409f7ebc9ff0f8cdb5bc9183c3
TheuZCoder <matheusrdsilva1@gmail.com>
2024-09-18 21:09:16 -0300

    atualizações

--- Projetos Java/Exercicios/exemplo_conteudo_base/src/main/java/br/com/theuz/ExemplosBasicos/notaAluno.java
+++ Projetos Java/Exercicios/exemplo_conteudo_base/src/main/java/br/com/theuz/ExemplosBasicos/notaAluno.java
@@ -1,12 +1,31 @@
 package br.com.theuz.ExemplosBasicos;
 
+import java.util.Scanner;
+
 public class notaAluno {
     public double nota1, nota2, nota3, nota4;
     public double media;
 
+
+    public void lerNotas() {
+        try (Scanner scanner = new Scanner(System.in)) {
+            System.out.println("Digite a primeira nota: ");
+            nota1 = scanner.nextDouble();
+            System.out.println("Digite a segunda nota: ");
+            nota2 = scanner.nextDouble();
+            System.out.println("Digite a terceira nota: ");
+            nota3 = scanner.nextDouble();
+            System.out.println("Digite a quarta nota: ");
+            nota4 = scanner.nextDouble();
+        } catch (Exception e) {
+            System.out.println("Erro ao ler as notas: " + e.getMessage());
+        }
+    }
+
     public void mediaNotas(double nota1, double nota2, double nota3, double nota4) {
         if (nota1 >= 8 || nota2 >= 8 || nota3 >= 8 || nota4 >= 8) {
             this.media = ((nota1 *= 1.1) + (nota2 *= 1.1) + (nota3 *= 1.1) + (nota4 *= 1.1)) / 4;
+            System.out.println("Aluno teve Bonus de 10% por conter notas acima de 8");
         } 
         this.media = (nota1 + nota2 + nota3 + nota4) / 4;
         if (this.media > 10) {
--- Projetos Java/Exercicios/exemplo_conteudo_base/src/main/java/br/com/theuz/Main.java
+++ Projetos Java/Exercicios/exemplo_conteudo_base/src/main/java/br/com/theuz/Main.java
@@ -1,27 +1,7 @@
 package br.com.theuz;
 
-import java.util.Scanner;
-
-import br.com.theuz.ExemplosBasicos.notaAluno;
-
 public class Main {
     public static void main(String[] args) {
-        int nota1, nota2, nota3, nota4;
-        notaAluno aluno;
         
-        try (Scanner scanner = new Scanner(System.in)) {
-            aluno = new notaAluno();
-            System.out.println("Digite a primeira nota: ");
-            nota1 = scanner.nextInt();
-            System.out.println("Digite a segunda nota: ");
-            nota2 = scanner.nextInt();
-            System.out.println("Digite a terceira nota: ");
-            nota3 = scanner.nextInt();
-            System.out.println("Digite a quarta nota: ");
-            nota4 = scanner.nextInt();
-        }
-
-        aluno.mediaNotas(nota1, nota2, nota3, nota4);
-        aluno.situacaoAluno(aluno.media);
     }
 }
